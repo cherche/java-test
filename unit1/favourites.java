efb47f07d8576bb7018d5c6d40cf9dd53d213293
Ryan Nguyen <contactryannguyen@gmail.com>
2018-09-07 10:04:13 -0400

    Begin first unit

---
+++ unit1/favourites.java
@@ -0,0 +1,25 @@
+public class favourites {
+  public static void main(String[] args) {
+    String[] answers = new String[]{
+      "My name is Ryan Nguyen.",
+      "My favourite colour is red.",
+      "My favourite TV show is Grey's Anatomy.",
+      "I don't really watch many movies.",
+      "My favourite computer game is Portal.",
+      "My favourite food is",
+      "Again, I don't really watch many movies.",
+      "My favourite group is Big Bang.",
+      "I guess I like to run, if you can count it as a sport.",
+      "I don't follow sports much.",
+      "My favourite books are those of the Clara Vine series by Jane Thynne.",
+      "My favourite subjects is mathematics.",
+      "I would like to visit Vancouver.",
+      "I have just one brother.",
+      "I took computer programming to explore potential career paths."
+    };
+
+    for (int i = 0; i < answers.length; i++) {
+      System.out.println(answers[i] + "\n");
+    }
+  }
+}
---
+++ unit1/name.java
@@ -0,0 +1,22 @@
+public class name {
+  public static void main(String[] args) {
+    new name();
+  }
+
+  public name() {
+    System.out.println("    ::::::::: :::   :::  :::    ::::    ::: ");
+    System.out.println("   :+:    :+::+:   :+::+: :+:  :+:+:   :+:  ");
+    System.out.println("  +:+    +:+ +:+ +:++:+   +:+ :+:+:+  +:+   ");
+    System.out.println(" +#++:++#:   +#++:+#++:++#++:+#+ +:+ +#+    ");
+    System.out.println("+#+    +#+   +#+ +#+     +#++#+  +#+#+#     ");
+    System.out.println("#+#    #+#   #+# #+#     #+##+#   #+#+#      ");
+    System.out.println("###    ###   ### ###     ######    ####       ");
+    System.out.println("    ::::    ::: :::::::: :::    ::::::   :::::::::::::::::    ::: ");
+    System.out.println("   :+:+:   :+::+:    :+::+:    :+::+:   :+::+:       :+:+:   :+:  ");
+    System.out.println("  :+:+:+  +:++:+       +:+    +:+ +:+ +:+ +:+       :+:+:+  +:+   ");
+    System.out.println(" +#+ +:+ +#+:#:       +#+    +:+  +#++:  +#++:++#  +#+ +:+ +#+    ");
+    System.out.println("+#+  +#+#+#+#+   +#+#+#+    +#+   +#+   +#+       +#+  +#+#+#     ");
+    System.out.println("#+#   #+#+##+#    #+##+#    #+#   #+#   #+#       #+#   #+#+#      ");
+    System.out.println("###    #### ########  ########    ###   #############    ####  ");
+  }
+}
