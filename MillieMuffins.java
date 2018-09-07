1ef5ad2d9e95037d0c08ffdf6174e4b10570c38c
Ryan Nguyen <contactryannguyen@gmail.com>
2018-09-06 21:26:19 -0400

    Push current progress

---
+++ MillieMuffins.java
@@ -0,0 +1,11 @@
+// If this looks like garbage, it's because I felt challenged to minimize the
+// number of lines of code, but that's a pretty meaningless metric anyway.
+// Real code golfers don't really need new lines.
+public class MillieMuffins {
+  public static void main(String[] args) {
+    System.out.print("Welcome to Millie's Marvelous Muffins!\n\n**If you like muffins, ours are marvelous!**\n\n");
+    int muffinCount = IBIO.inputInt("How many muffins would you like? ");
+    int costPerMuffin = (muffinCount / 10 >= 4) ? 1 : (new int[]{5, 3, 2, 2})[muffinCount / 10];
+    System.out.print("The total cost per muffin is $" + costPerMuffin + "\nThe total cost is $" + costPerMuffin * muffinCount + "\n");
+  }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# java-test
+A huge mess of lazily-learnt Java for my computer studies course.
