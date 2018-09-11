e8d8e0086c5415a8ac7b3f21eee711dc5660cd70
Ryan Nguyen <contactryannguyen@gmail.com>
2018-09-11 10:22:36 -0400

    Remove unnecessary abstraction

--- 1/03/MadLib.java
+++ 1/03/MadLib.java
@@ -1,23 +1,49 @@
 public class MadLib {
-  private String[] descs;
-  private String template;
+  public static void main(String[] args) {
+    String[] descs = new String[] {
+      "a made-up name",
+      "a colour",
+      "another colour",
+      "a number",
+      "another number"
+    };
+    // Oh my god. It's disgusting. It's 1-based instead of 0-based;
+    String template =
+      "A Strange Animal.....\n"
+      + "The %1$s is an animal\n"
+      + "that has %2$s fur with\n"
+      + "%3$s spots on its tail. An\n"
+      + "adult %1$s may weigh over\n"
+      + "%4$s pounds and stand over %5$s\n"
+      + "feet tall.";
+    /*
+    for (int i = 0; i < descs.length; i++) {
+      String word = IBIO.inputString("Enter " + descs[i] + ": ");
+      words[i] = word;
+    }
+    */
+    String[][] wordSets = new String[][] {
+      {
+        "Ooklum",
+        "green",
+        "magenta",
+        "3",
+        "45"
+      },
+      {
+        "Gahoom",
+        "blue",
+        "yellow",
+        "15",
+        "2"
+      }
+    };
 
-  public MadLib(String[] descs, String template) {
-    this.descs = descs;
-    this.template = template;
-  }
-
-  public String[] getDescs() {
-    return descs;
-  }
-
-  public String getTemplate() {
-    return template;
-  }
-
-  public String getFilledStory(String[] words) {
-    // If we don't cast to Object[], the compiler throws a fit
-    String story = String.format(template, (Object[]) words);
-    return story;
+    for (int i = 0; i < wordSets.length; i++) {
+      String[] words = wordSets[i];
+      String story = String.format(template, (Object[]) words);
+      System.out.println(story);
+      System.out.println();
+    }
   }
 }
