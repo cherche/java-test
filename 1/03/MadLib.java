7b574c849bd079594398ae4f58c7ca549eff2213
Ryan Nguyen <contactryannguyen@gmail.com>
2018-09-10 21:10:46 -0400

    Added additional getter for debugging purposes

--- 1/03/MadLib.java
+++ 1/03/MadLib.java
@@ -11,7 +11,12 @@ public class MadLib {
     return descs;
   }
 
+  public String getTemplate() {
+    return template;
+  }
+
   public String getFilledStory(String[] words) {
+    // If we don't cast to Object[], the compiler throws a fit
     String story = String.format(template, (Object[]) words);
     return story;
   }
