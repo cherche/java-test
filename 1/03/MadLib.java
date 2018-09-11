5142aa3d6e63976ce94cdfdd5521b9a236dc5562
Ryan Nguyen <contactryannguyen@gmail.com>
2018-09-10 21:07:56 -0400

    Rewrote to use String.format() method

--- 1/03/MadLib.java
+++ 1/03/MadLib.java
@@ -1,11 +1,10 @@
 public class MadLib {
   private String[] descs;
-  private TempPair[] template;
-  private int[] indices;
+  private String template;
 
-  public MadLib(String[] descs, TempPair[] template) {
+  public MadLib(String[] descs, String template) {
+    this.descs = descs;
     this.template = template;
-    this.indices = indices;
   }
 
   public String[] getDescs() {
@@ -13,20 +12,7 @@ public class MadLib {
   }
 
   public String getFilledStory(String[] words) {
-    int i = 0;
-    String story = "";
-
-    for (; i < template.length; i++) {
-      String string = template[i].string;
-      int index = template[i].index;
-      if (index == -1) {
-        story += string;
-      } else {
-        String word = words[index];
-        story += string + word;
-      }
-    }
-
+    String story = String.format(template, (Object[]) words);
     return story;
   }
 }
--- 1/03/test.java
+++ 1/03/test.java
@@ -7,28 +7,44 @@ public class test {
       "a number",
       "another number"
     };
-    String[] words = new String[descs.length];
-    TempPair[] template = new TempPair[] {
-      new TempPair("A Strange Animal..... \nThe ", 0),
-      new TempPair(" is an animal \nthat has ", 1),
-      new TempPair(" fur with \n", 2),
-      new TempPair(" spots on its tail. An \nadult ", 0),
-      new TempPair(" may weigh over \n", 3),
-      new TempPair(" pounds and stand over ", 4),
-      // I really couldn't think of a better way to make
-      // the code more readable
-      // I guess we'll have to settle for this awkward -1
-      new TempPair("\nfeet tall.", -1)
-    };
+    // Oh my god. It's disgusting. It's 1-based instead of 0-based;
+    String template =
+      "A Strange Animal.....\n"
+      + "The %1$s is an animal\n"
+      + "that has %2$s fur with\n"
+      + "%3$s spots on its tail. An\n"
+      + "adult %1$s may weigh over\n"
+      + "%4$s pounds and stand over %5$s\n"
+      + "feet tall.";
     MadLib madlib = new MadLib(descs, template);
-
+    /*
     for (int i = 0; i < descs.length; i++) {
       String word = IBIO.inputString("Enter " + descs[i] + ": ");
       words[i] = word;
     }
+    */
+    String[][] wordSets = new String[][] {
+      {
+        "Ooklum",
+        "green",
+        "magenta",
+        "3",
+        "45"
+      },
+      {
+        "Gahoom",
+        "blue",
+        "yellow",
+        "15",
+        "2"
+      }
+    };
 
-    String story = madlib.getFilledStory(words);
-    System.out.println("");
-    System.out.println(story);
+    for (int i = 0; i < wordSets.length; i++) {
+      String[] words = wordSets[i];
+      String story = madlib.getFilledStory(words);
+      System.out.println(story);
+      System.out.println();
+    }
   }
 }
