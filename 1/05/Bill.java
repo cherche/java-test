800812c804276987a039e467a97157745341e01d
Ryan Nguyen <contactryannguyen@gmail.com>
2018-09-12 20:31:42 -0400

    Changed output formatting in Bill.java

--- 1/05/Bill.java
+++ 1/05/Bill.java
@@ -4,19 +4,25 @@ public class Bill {
     // I don't think there's a way around it though
     double tab = IBIO.inputDouble("What is the dinner tab? $");
     double tax = tab * 0.13;
-    double tipRate = IBIO.inputDouble("What is the tip percentage? ");
+    double tipPercentage = IBIO.inputDouble("What is the tip percentage? ");
     // Also, I think using this IBIO class means that I don't really
     // understand Java. I should really write my own class.
-    double tip = tab * tipRate / 100;
+    double tip = tab * tipPercentage / 100;
     double bill = tab + tax + tip;
-    IBIO.output("The bill is $" + round(bill, 2));
+    // Occasionally, the double will be a perfect whole
+    // or a perfect tenth, but the displayed price
+    // should include two decimal places all the time
+    IBIO.output(
+      String.format("The bill is $%.2f", ceil(bill, 2))
+    );
     int diners = IBIO.inputInt("How many diners are there? ");
-    // Technically, this should be rounded up to the nearest cent
-    IBIO.output("Each diner should pay $" + round(bill / diners, 2));
+    IBIO.output(
+      String.format("Each diner should pay $%.2f", ceil(bill / diners, 2))
+    );
   }
 
-  static double round(double num, int places) {
+  static double ceil(double num, int places) {
     double roundFactor = Math.pow(10, places);
-    return Math.round(num * roundFactor) / roundFactor;
+    return Math.ceil(num * roundFactor) / roundFactor;
   }
 }
