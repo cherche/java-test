1cd5e55d507bcbd11366c04baaa4fede1307f0ab
Ryan Nguyen <contactryannguyen@gmail.com>
2018-11-28 10:41:29 -0500

    Finished CommonSense program

---
+++ 5/01/CommonSense.java
@@ -0,0 +1,65 @@
+public class CommonSense {
+  public static void main(String[] args) {
+    String[] ROMAN_NUMERALS = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
+    for (int i = 0; i < ROMAN_NUMERALS.length; i++) {
+      System.out.println(ROMAN_NUMERALS[i]);
+    }
+    int[] FIBONACCI_SEQUENCE = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55};
+    for (int i = 0; i < FIBONACCI_SEQUENCE.length; i++) {
+      System.out.println(FIBONACCI_SEQUENCE[i]);
+    }
+    String[] RAINBOW_COLOURS = {
+      "Red",
+      "Orange",
+      "Yellow",
+      "Green",
+      "Blue",
+      "Indigo",
+      "Violet"
+    };
+    for (int i = 0; i < RAINBOW_COLOURS.length; i++) {
+      System.out.println(RAINBOW_COLOURS[i]);
+    }
+    String[] NUMBERS = {
+      "Zero",
+      "One",
+      "Two",
+      "Three",
+      "Four",
+      "Five",
+      "Six",
+      "Seven",
+      "Eight",
+      "Nine",
+      "Ten"
+    };
+    for (int i = 0; i < NUMBERS.length; i++) {
+      System.out.println(NUMBERS[i]);
+    }
+    String[] BODY_PARTS = {
+      "Head",
+      "Shoulders",
+      "Knees",
+      "Toes",
+      "Eyes",
+      "Ears",
+      "Mouth",
+      "Nose"
+    };
+    for (int i = 0; i < BODY_PARTS.length; i++) {
+      System.out.println(BODY_PARTS[i]);
+    }
+    String[] PERIOD_TIMES = {"8:15", "9:40", "10:59", "12:14", "1:33"};
+    for (int i = 0; i < PERIOD_TIMES.length; i++) {
+      System.out.println(PERIOD_TIMES[i]);
+    }
+    int[] SQUARES = {1, 4, 9, 16, 25, 36, 49, 64, 81, 100};
+    for (int i = 0; i < SQUARES.length; i++) {
+      System.out.println(SQUARES[i]);
+    }
+    char[] ALPHABET = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'};
+    for (int i = 0; i < ALPHABET.length; i++) {
+      System.out.println(ALPHABET[i]);
+    }
+  }
+}
